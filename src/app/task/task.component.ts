dadb4b2d658b97e31f4b4b979073dd735c312764
Filip Mucko <mucko.fil@ad.meelogic.com>
2019-05-21 10:39:24 +0200

    lost file found

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,74 @@
+import { TaskService } from '../task.service';
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+  page: number = 0;
+  tasks: Array<Task>;
+  pages: Array<number>;
+  key: string = 'name';
+  reverse: boolean = false;
+
+  taskToBeCreated: Task;
+
+  p: number = 1;
+
+  constructor(private _myService: TaskService) {
+  }
+
+  ngOnInit() {
+    this.getAllTasks();
+  }
+
+  sort(key: string) {
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+
+  getAllTasks() {
+    this._myService.getAllTasks().subscribe(
+      data => {
+        this.tasks = data;
+      })
+  }
+
+  removeTask(task: Task) {
+    this._myService.removeTask(task.id).subscribe(
+      data => {
+        if (data === task.id) {
+          this.tasks = this.tasks.filter(function (_task) {
+            return _task.id !== task.id;
+          })
+        }
+      }
+    );
+  }
+
+  addNewTask(name: string, description: string) {
+    this._myService.addNewTask(name, description).subscribe(
+      task => { this.tasks.push(task) }
+    );
+  }
+
+  startProcessingTask(task: Task) {
+    this._myService.startProcessingTask(task).subscribe(
+      _task => { task.assignValuesOf(_task); }
+      // jeszcze jakiś popup informujący o dokonanaje zmianie (to samo w pozostałych metodach) - czyli implementacja jakiegoś equals
+    );
+  }
+
+  cancelProcessingTask(task: Task) {
+    this._myService.cancelProcessingTask(task).subscribe(
+      _task => { task.assignValuesOf(_task); }
+      // jeszcze jakiś popup informujący o dokonanaje zmianie (to samo w pozostałych metodach) - czyli implementacja jakiegoś equals
+    );
+  }
+}
+
+//TODO: obsługa błędów które dostaję od api powinny się odbywać w jakimś httpHandlerze, zeby nie duplikować kodu w każdym subscribe
+
